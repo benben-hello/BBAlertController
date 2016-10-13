978ede116eb3eda5263c6ec79bc00291941397ff
benben_hello <benben_hello@163.com>
2016-10-13 09:53:19 +0800

    first commit

--- BBAlertController/ViewController.m
+++ BBAlertController/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "UIAlertController+Color.h"
 
 @interface ViewController ()
 
@@ -17,6 +18,36 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)buttonClick:(UIButton *)sender {
+    
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"自定义提示的字体颜色" message:@"我是描述信息" preferredStyle:UIAlertControllerStyleActionSheet];
+    
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    [alertController addAction:cancelAction];
+    
+    UIAlertAction *otherAction = [UIAlertAction actionWithTitle:@"其他" style:UIAlertActionStyleDefault handler:nil];
+    [alertController addAction:otherAction];
+    
+    //修改标题颜色和描述信息颜色
+    if (sender.tag == 1) {
+        alertController.titleColor = [UIColor redColor];
+        alertController.messageColor = [UIColor greenColor];
+    }
+    
+    //修改单个按钮字体颜色
+    if (sender.tag == 2) {
+        cancelAction.textColor = [UIColor redColor];
+        otherAction.textColor = [UIColor greenColor];
+    }
+    
+    //修改所有按钮颜色
+    if (sender.tag == 3) {
+        alertController.tintColor = [UIColor greenColor];
+    }
+    
+    [self presentViewController:alertController animated:YES completion:nil];
 }
 
 
